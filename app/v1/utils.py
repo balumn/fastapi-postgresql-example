30fff56cf7517f9b1d49d784a830ff71ab14c933
Balu M N <balu.mn@scudnetworks.com>
2023-07-31 16:45:49 +0530

    removing the validators as they are not required

--- app/v1/utils.py
+++ app/v1/utils.py
@@ -1,7 +1,4 @@
 from pydantic import BaseModel
-import re
-
-REGX_EMAIL = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
 
 
 class CommonResponse(BaseModel):
@@ -13,9 +10,3 @@ class CommonResponse(BaseModel):
         json_schema_extra = {
             "example": {"status": "failure", "message": "API failed", "data": None}
         }
-
-
-def validate_email(cls, v):
-    if not re.match(REGX_EMAIL, v) and 7 < len(v) < 320:
-        raise ValueError("Invalid Email!")
-    return v
